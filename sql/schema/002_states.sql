28b6636146770d3a8c21f8db220d014543b4aa27
Nazif Barassounon <nazifbarassounon@gmail.com>
2026-07-06 10:52:45 +0100

    migration: add the created_at and updated_at columns to the states table

--- sql/schema/002_states.sql
+++ sql/schema/002_states.sql
@@ -3,6 +3,8 @@ CREATE TABLE states (
     id UUID PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
     board_id UUID NOT NULL,
     CONSTRAINT fk_board FOREIGN KEY (board_id) REFERENCES boards(id) ON DELETE CASCADE
 );
