f80a5f4eb47e86bd2c6d0c79c3f6e15e8103aedb
Nazif Barassounon <nazifbarassounon@gmail.com>
2026-08-23 15:18:16 +0100

    move the dbToTask function to the tasks package

--- internal/tasks/types.go
+++ internal/tasks/types.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/nazifbara/kanban-api/internal/database"
 )
 
 type Task struct {
@@ -29,4 +30,25 @@ type UpdateParam struct {
 	Title       *string    `json:"title"`
 	Description *string    `json:"description"`
 	Position    *int32     `json:"position"`
+}
+
+func DBToTaskSlice(dbTasks []database.Task) []Task {
+	tasks := []Task{}
+	for _, dbTask := range dbTasks {
+		tasks = append(tasks, DBToTask(dbTask))
+	}
+	return tasks
+}
+
+func DBToTask(dbTask database.Task) Task {
+	return Task{
+		ID:          dbTask.ID,
+		BoardID:     dbTask.BoardID,
+		ColumnID:    dbTask.ColumnID,
+		Title:       dbTask.Title,
+		Description: dbTask.Description.String,
+		CreatedAt:   dbTask.CreatedAt,
+		UpdatedAt:   dbTask.UpdatedAt,
+		Position:    int(dbTask.Position),
+	}
 }
