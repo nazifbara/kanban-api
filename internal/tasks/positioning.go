4cb6115f0100bd3ecefde8acf498611d232a1e77
Nazif Barassounon <nazifbarassounon@gmail.com>
2026-08-23 19:36:44 +0100

    fix: remove the positionTask call before shiftTaskFrom

--- internal/tasks/positioning.go
+++ internal/tasks/positioning.go
@@ -9,36 +9,7 @@ import (
 	"github.com/google/uuid"
 	"github.com/nazifbara/kanban-api/internal/apierrors"
 	"github.com/nazifbara/kanban-api/internal/database"
-	"github.com/nazifbara/kanban-api/internal/utils"
 )
-
-func PositionTask(ctx context.Context, q *database.Queries, taskID, columnID uuid.UUID, newPosition int) error {
-	destinationTasks, err := q.GetColumnTasksForUpdate(ctx, columnID)
-	if err != nil {
-		return apierrors.FromDBErr(err)
-	}
-	err = utils.PositionItem(ctx, utils.PositionParam[database.Task]{
-		Items:    destinationTasks,
-		ItemID:   func(t database.Task) uuid.UUID { return t.ID },
-		ItemPos:  func(t database.Task) int { return int(t.Position) },
-		TargetID: taskID,
-		Position: newPosition,
-		UpdateItem: func(ctx context.Context, id uuid.UUID, pos int32) error {
-			err := q.UpdateTaskPosition(ctx, database.UpdateTaskPositionParams{
-				ID:       id,
-				Position: pos,
-			})
-			if err != nil {
-				return err
-			}
-			return nil
-		},
-	})
-	if err != nil {
-		return err
-	}
-	return nil
-}
 
 func ChangeTaskColumn(ctx context.Context, q *database.Queries, task database.Task, columnID uuid.UUID, newPosition int) error {
 	oldColumnTasks, err := q.GetColumnTasksForUpdate(
@@ -61,7 +32,6 @@ func ChangeTaskColumn(ctx context.Context, q *database.Queries, task database.Ta
 			return apierrors.FromDBErr(err)
 		}
 	}
-	err = PositionTask(ctx, q, task.ID, columnID, newPosition)
 	err = q.ShiftTasksFrom(
 		ctx,
 		database.ShiftTasksFromParams{
